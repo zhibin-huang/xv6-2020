6ad19e18bffa593e26814e77dbb9b98f44eca848
Zhibin Huang <zhibinhwang@gmail.com>
2021-09-19 04:52:28 +0800

    my solution to lazyallocation lab

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,19 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if(r_scause()  == 13 || r_scause() == 15){
+    //page fault (when load and store data)
+    uint64 begin  = PGROUNDDOWN(r_stval());
+    uint64 end = begin + PGSIZE; 
+    if(begin >= p->sz || begin < PGROUNDUP(p->trapframe->sp)){
+      p->killed = 1;
+    }
+    else{
+      if(uvmalloc(p->pagetable, begin, end) == 0){
+        p->killed =1;
+      }
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
